22ea9d3b77a8e589ea6368bc57cf4a0265cb4f9d
Zahid2297 <techpirate97@gmail.com>
2025-11-11 19:19:18 +0530

    add gsap

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useRef } from "react"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
 const HomePage = () =>{
-    return <div><h1>yaseen</h1></div>
+    const circle = useRef();
+
+   useGSAP(()=>{
+    gsap.to(circle.current, {x:400, duration: 2, borderRadius: "50%"})
+    console.log(circle)
+   })
+    return <div ref={circle} className="w-100 h-100 bg-red-700">
+
+    </div>
 
 }
 

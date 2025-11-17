89d9f2e03b4edd1118d4a154e2643f81f0ccaf1d
Zahid2297 <techpirate97@gmail.com>
2025-11-17 20:50:56 +0530

    add 3d model on landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import ModelViewer from "./ModelViewer";
+import { Fullscreen } from "lucide-react";
 
 const Hero = () => {
   return (
-    <section className="relative md:pt-44 pt-28 text-white">
-      <div className="container mx-auto max-w-6xl px-4 grid grid-cols-12 gap-4 relative z-10">
+    <section className="relative md:pt-34 pt-18 text-white">
+      <div className="container  mx-auto  grid grid-cols-12 gap-4 relative z-10">
         {/* LEFT SIDE */}
         <div
           className="md:col-span-6 col-span-12 p-4 md:px-4 px-0 space-y-4 flex flex-col items-start justify-center"
-          data-aos="fade-right"
-          data-aos-delay="200"
-          data-aos-duration="1000"
+          data-aos="fade-up"
+          data-aos-delay="2000"
+          data-aos-duration="2000"
         >
           <div className="flex gap-2 items-center">
             <span className="w-3 h-3 rounded-full bg-success"></span>
@@ -33,10 +35,28 @@ const Hero = () => {
 
         {/* RIGHT SIDE IMAGE */}
         <div className="md:col-span-6 col-span-12 relative ">
-          <img
-            src="./src/assets/img1.jpg"
+          {/* <img
+            src="/img1.jpg"
             alt="hero-banner"
             className="w-full h-auto rounded-lg object-cover"
+          /> */}
+          <ModelViewer
+            url="/view.glb"
+            width={1200}
+            height={900}
+            showScreenshotButton={false}
+            enableHoverRotation={true}
+            fadeIn={true}
+            autoRotate={false}
+            autoRotateSpeed={0.2}
+            defaultRotationY={35}
+            defaultRotationX={18}
+            defaultZoom={3} // closer
+            minZoomDistance={0}
+            maxZoomDistance={8}
+            modelXOffset={3} // left-right
+            modelYOffset={-9} // up-down
+            enableManualRotation={true}
           />
         </div>
       </div>
